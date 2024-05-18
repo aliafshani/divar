c1a194c941407b85fc870cc75c4a083a9fbc3758
aliwsh <111402933+aliafshani@users.noreply.github.com>
2024-05-18 09:59:36 -0700

    build something new in navbar

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+
+const router = [
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+];
+
+
+export default router;
